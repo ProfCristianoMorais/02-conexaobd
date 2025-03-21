be7698b57595e390c23a37fff1ca04fce0a0a39c
prof.cristianomorais <prof.cristianomorais@gmail.com>
2025-03-21 11:10:52 -0300

    Aula primeiro CRUD

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php 
+//Instanciando a classe PDO
+$pdo = new PDO("mysql:dbname=projetoETC;host=localhost", "root");
+//Faz a consulta na tabela usuários do banco de dados
+$sql = $pdo->query('SELECT * FROM usuarios');
+//Conta quantas linhas de registro
+echo "TOTAL: ".$sql->rowCount();
+//Cria uma variavel dados e associada cada uma delas
+$dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+//Pre configura a saida
+echo '<pre>';
+print_r($dados);
+ 
